f800c41b0e6bb7e7586e986b671ec740f4406e79
denjae <dennis.jga@gmail.com>
2013-12-10 11:54:42 +0100

    erste Funktionen DiaServive implementiert

--- src/main/java/diaPublisher/DiabeticService.java
+++ src/main/java/diaPublisher/DiabeticService.java
@@ -1,7 +1,20 @@
 package diaPublisher;
 
+import java.io.File;
+
 /**
  * Created by denjae on 09.12.13.
  */
 public class DiabeticService {
+
+
+    public File getBZ (String Name){
+        File bz = null;
+       return bz;
+    }
+
+    public void setBZ(String name){
+
+    }
+
 }
