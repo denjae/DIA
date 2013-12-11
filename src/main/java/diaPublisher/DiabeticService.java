8e99e13d1b89ff2e429841025a793ee85c415284
denjae <dennis.jga@gmail.com>
2013-12-11 16:15:38 +0100

    getBZ() Auslesen der Knotenanzahl moeglich

--- src/main/java/diaPublisher/DiabeticService.java
+++ src/main/java/diaPublisher/DiabeticService.java
@@ -2,11 +2,14 @@ package diaPublisher;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by denjae on 09.12.13.
@@ -21,11 +24,32 @@ public class DiabeticService {
     public File getBZ(String user) throws ParserConfigurationException {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        try {
+            docBuilder.parse("/Users/denjae/git/DIA/src/main/resources/"+ user +".xml");
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Document document = docBuilder.newDocument();
-        Element rootElement = document.createElement("BZ");
+        //Ermitteln von max 20 Knoten
+        NodeList list = document.getElementsByTagName("BZeintrag");
+        int length = 0;
+            if(list.getLength()<20) {
+        length = list.getLength();
+            }
+            else {
+                length= 20;
+            }
+
+
+
+
 
         File currentBZ = null;
-        File patbz = new File("\"/Users/denjae/git/DIA/src/main/resources/"+ user +".xml");
+
+
+
         return currentBZ;
     }
 
