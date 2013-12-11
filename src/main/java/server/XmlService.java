47160b78b9a8709a1ca72e13183b1661ed9013ce
denjae <dennis.jga@gmail.com>
2013-12-11 11:38:01 +0100

    Test entwickelt, createFile weiterentwickelt

--- src/main/java/server/XmlService.java
+++ src/main/java/server/XmlService.java
@@ -5,19 +5,46 @@ import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 
 /**
  * Created by denjae on 10.12.13.
  */
 public class XmlService {
-
+     //TODO: Überlegen wie Name des Files erzeugt wird
     //Erzeugt und Speichert ein neues XML-File
-    public void createFile() throws Exception{
-    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+    public void createFile() throws Exception {
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
+        Document document = docBuilder.newDocument();
+        Element rootElement = document.createElement("BZ");
 
-    Document document = docBuilder.newDocument();
-    Element rootElement = document.createElement("BZ");
-}
+        Element name = document.createElement("Name");
+        rootElement.appendChild(name);
+
+        Element entry = document.createElement("BZeintrag");
+        rootElement.appendChild(entry);
+
+        Element bz = document.createElement("Blutzucker");
+        entry.appendChild(bz);
+
+        Element time = document.createElement("Uhrzeit");
+        entry.appendChild(time);
+
+        Element date = document.createElement("Datum");
+        entry.appendChild(date);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(document);
+        StreamResult result = new StreamResult(new File("/Users/denjae/git/DIA/src/main/resources/test.xml"));
+
+        transformer.transform(source, result);
+        System.out.println("File saved!");
+    }
 }
---
+++ src/test/java/XmlServiceTest.java
@@ -0,0 +1,18 @@
+import server.XmlService;
+
+/**
+ * Created by denjae on 11.12.13.
+ */
+public class XmlServiceTest {
+
+    public static void main(String[] args) {
+
+    XmlService test = new XmlService();
+        try {
+            test.createFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
