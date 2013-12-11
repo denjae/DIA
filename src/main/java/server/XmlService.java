9a26a04453514b48e8e63d29dffe705051ef0727
denjae <dennis.jga@gmail.com>
2013-12-11 14:06:59 +0100

    createFile überarbeitet, funktioniert nun

--- src/main/java/server/XmlService.java
+++ src/main/java/server/XmlService.java
@@ -1,11 +1,14 @@
 package server;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -15,36 +18,68 @@ import java.io.File;
  * Created by denjae on 10.12.13.
  */
 public class XmlService {
-     //TODO: Überlegen wie Name des Files erzeugt wird
-    //Erzeugt und Speichert ein neues XML-File
-    public void createFile() throws Exception {
-        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+     //TODO: Überlegen wie sinnvoll ein Name erzeugt wird
+    public void createFile(String user) {
 
-        Document document = docBuilder.newDocument();
-        Element rootElement = document.createElement("BZ");
+        try {
 
-        Element name = document.createElement("Name");
-        rootElement.appendChild(name);
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
 
-        Element entry = document.createElement("BZeintrag");
-        rootElement.appendChild(entry);
+            // Root Element
+            Document document = docBuilder.newDocument();
+            Element rootElement = document.createElement("BZ");
+            document.appendChild(rootElement);
 
-        Element bz = document.createElement("Blutzucker");
-        entry.appendChild(bz);
+            // Element Name
+            Element name = document.createElement("Name");
+            rootElement.appendChild(name);
 
-        Element time = document.createElement("Uhrzeit");
-        entry.appendChild(time);
+            // Element Name
+            Element entry = document.createElement("BZeintrag");
+            rootElement.appendChild(entry);
 
-        Element date = document.createElement("Datum");
-        entry.appendChild(date);
+            // set attribute to staff element
+           // Attr attr = document.createAttribute("id");
+            //attr.setValue("1");
+           // name.setAttributeNode(attr);
 
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(document);
-        StreamResult result = new StreamResult(new File("/Users/denjae/git/DIA/src/main/resources/test.xml"));
+            // shorten way
+            // staff.setAttribute("id", "1");
 
-        transformer.transform(source, result);
-        System.out.println("File saved!");
+            // Element Blutzucker
+            Element bz = document.createElement("Blutzucker");
+           // bz.appendChild(document.createTextNode("yong"));
+            entry.appendChild(bz);
+
+            // Element Uhrzeit
+            Element time = document.createElement("lastname");
+            //lastname.appendChild(document.createTextNode("mook kim"));
+            entry.appendChild(time);
+
+            // Element Datum
+            Element nickname = document.createElement("nickname");
+           // nickname.appendChild(document.createTextNode("mkyong"));
+            entry.appendChild(nickname);
+
+
+            // write the content into xml file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File("/Users/denjae/git/DIA/src/main/resources/"+user+".xml"));
+
+            // Output to console for testing
+            // StreamResult result = new StreamResult(System.out);
+
+            transformer.transform(source, result);
+
+            System.out.println("File saved!");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        }
     }
 }
--- src/test/java/XmlServiceTest.java
+++ src/test/java/XmlServiceTest.java
@@ -9,7 +9,7 @@ public class XmlServiceTest {
 
     XmlService test = new XmlService();
         try {
-            test.createFile();
+            test.createFile("Denjae");
         } catch (Exception e) {
             e.printStackTrace();
         }
