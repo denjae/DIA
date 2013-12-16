8054cd04947b74627d41394ed3b3534bb5bb7be8
denjae <dennis.jga@gmail.com>
2013-12-16 15:02:17 +0100

    getBZ() Neuimplementierung

--- src/main/java/diaPublisher/DiabeticService.java
+++ src/main/java/diaPublisher/DiabeticService.java
@@ -1,22 +1,13 @@
 package diaPublisher;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by denjae on 09.12.13.
@@ -25,14 +16,14 @@ public class DiabeticService {
 
 
     // Erstellt XML mit den maximal 20 zuletzt eingetragenen Werten und gibt diese zurück
-  /*  public void getBZ(String user)  {
+    /*public void getBZ(String user) {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder docBuilder = null;
         try {
             docBuilder = docFactory.newDocumentBuilder();
 
-           Document document = docBuilder.parse("/Users/denjae/git/DIA/src/main/resources/" + user + ".xml");
-           Document lastValues = docBuilder.newDocument();
+            Document document = docBuilder.parse("/Users/denjae/git/DIA/src/main/resources/" + user + ".xml");
+            Document lastValues = docBuilder.newDocument();
 
             // Root Element
             Element rootElement = lastValues.createElement("BZ");
@@ -44,37 +35,33 @@ public class DiabeticService {
             rootElement.appendChild(name);
 
 
-        //Neues XML-Dokument mit den max. 20 letzten Werten
+            //Neues XML-Dokument mit den max. 20 letzten Werten
             //Ermitteln von max 20 Knoten
             NodeList list = document.getElementsByTagName("BZeintrag");
             int length = 0;
-            if(list.getLength()<20) {
+            if (list.getLength() < 20) {
                 length = list.getLength();
-            }
-            else {
-                length= 20;
+            } else {
+                length = 20;
             }
 
-            for (int i = 0; i < length ; i++) {
+            for (int i = 0; i < length; i++) {
                 // Element Eintrag
-              // Element entry = lastValues.createElement("BZeintrag");
-              //rootElement.appendChild(entry);
-              Node node = lastValues.importNode(document.getDocumentElement(),true);
-              lastValues.getDocumentElement().appendChild(node);
-                System.out.println(1);
-             }
+                // Element entry = lastValues.createElement("BZeintrag");
+                //rootElement.appendChild(entry);
+                System.out.println();
+            }
 
 
+            // Inhalt in XML speichern
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(lastValues);
+            StreamResult result = new StreamResult(new File("/Users/denjae/git/DIA/src/main/resources/returnBz.xml"));
+            transformer.transform(source, result);
 
-        // Inhalt in XML speichern
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
-        Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(lastValues);
-        StreamResult result = new StreamResult(new File("/Users/denjae/git/DIA/src/main/resources/returnBz.xml"));
-        transformer.transform(source, result);
-
-        System.out.println("File saved!");
-        transformer.transform(source, result);
+            System.out.println("File saved!");
+            transformer.transform(source, result);
         } catch (SAXException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -87,14 +74,47 @@ public class DiabeticService {
             e.printStackTrace();
         }
 
+    }*/
+
+    public void getBZ(String user) throws JDOMException, IOException {
+        SAXBuilder builder = new SAXBuilder();
+        File xmlFile = new File("/Users/denjae/git/DIA/src/main/resources/" + user + ".xml");
+
+        try {
+            Document document = (Document) builder.build(xmlFile);
+            Document lastEntries = new Document();
+            Element rootNode= document.getRootElement();
+            Element BZ = new Element("BZ");
+            document.setRootElement(BZ);
+            List list = rootNode.getChildren("BZeintrag");
+
+            //Begrenzt Ergebnis auf die max 20 letzten Werte
+            int length = 0;
+            if (list.size() < 20) {
+                length = list.size();
+            } else {
+                length = 20;
+            }
+
+            for (int i = 0; i < length; i++) {
+
+                Element node = (Element) list.get(i);
+
+                System.out.println("Blutzucker : " + node.getChildText("Blutzucker"));
+                System.out.println("Uhrzeit : " + node.getChildText("Uhrzeit"));
+                System.out.println("Datum : " + node.getChildText("Datum"));
+
+
+            }
+
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        } catch (JDOMException jdomex) {
+            System.out.println(jdomex.getMessage());
+        }
     }
 
-    */
 
-    public void getBZ(String user){
-
-
-    }
 
 
     public void setBZ(String name) {
