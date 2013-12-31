cdcfb1047b6f93d0b981b9d4a65d802a2b09555e
denjae <dennis.jga@gmail.com>
2013-12-31 11:37:19 +0100

    Beginn der GUI-Entwicklung

---
+++ src/main/java/gui/Login.java
@@ -0,0 +1,15 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by denjae on 31.12.13.
+ */
+public class Login {
+    private JLabel Header;
+    private JTextPane textPane1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
