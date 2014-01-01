a78bd90b11a8fab14fb56681092993aee4a593ba
denjae <dennis.jga@gmail.com>
2014-01-01 14:08:31 +0100

    Login Funktionen teilweise implementiert

--- src/main/java/gui/Login.java
+++ src/main/java/gui/Login.java
@@ -11,6 +11,41 @@ import java.awt.event.ItemListener;
  */
 public class Login {
     private JPanel panel1;
+    private JLabel Header;
+    private JButton login;
+    private JComboBox ChoosingUser;
+    private JTextField benutzernameTextField;
+    private int selectedUser = 0;
+
+    public Login() {
+        ChoosingUser.addItemListener(new ItemListener() {
+
+                public void itemStateChanged( ItemEvent event ) {
+                if( event.getStateChange() == ItemEvent.SELECTED ) {
+                    selectedUser = ChoosingUser.getSelectedIndex();
+                    System.out.println(selectedUser);
+                }
+            }
+        });
+        login.addActionListener(new ActionListener() {
+
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               /* if(selectedUser == 0){
+                    Diabetic diabetic = new Diabetic();
+                    diabetic.setName(name);
+                }
+                else{
+                    Doctor doctor = new Doctor();
+                    doctor.setName(name);
+                }*/
+            }
+
+        });
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Login");
@@ -18,30 +53,5 @@ public class Login {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-    }
-
-    private JLabel Header;
-    private JButton Einloggen;
-    private JComboBox ChoosingUser;
-
-    public Login() {
-        ChoosingUser.addItemListener(new ItemListener() {
-            String selectedItem = (String) ChoosingUser.getSelectedItem();
-
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-            }
-        });
-        Einloggen.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-    }
-
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
     }
 }
