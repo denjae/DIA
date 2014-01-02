ca5611b86d3d633c237ec59dacadb8ac72f11f6c
denjae <dennis.jga@gmail.com>
2014-01-02 16:54:09 +0100

    Beginn Entwicklung Diabetiker-GUI

---
+++ src/main/java/gui/Diabetic.java
@@ -0,0 +1,12 @@
+package gui;
+
+import javax.swing.*;
+
+/**
+ * Created by denjae on 02.01.14.
+ */
+public class Diabetic {
+    private JLabel DiaLabel;
+    private JTextField bloodSugar;
+    private JTable table1;
+}
